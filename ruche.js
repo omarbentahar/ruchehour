2b171d2d3a0f2388f886f5b6bfa00b1938a70b95
omarbentahar <113634340+omarbentahar@users.noreply.github.com>
2022-09-15 23:36:46 -0700

    Add files via upload

--- ruche.js
+++ ruche.js
@@ -1,2 +1,19 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+var gauche = document.getElementById('Gauche');
+var droite = document.getElementById('Droite');
+
+function blockimages(alternate) {
+    if (alternate) {
+        return `<div> <input type="image" src="icones/ruche.png"/></div><div><input type="image" src="icones/abeille.png"/></div>`
+    }
+    else {
+        return `<div> <input type="image" src="icones/abeille.png"/></div><div><input type="image" src="icones/ruche.png"/></div>`
+    }
+}
+
+for (var i = 0; i < 10; i++) {
+    gauche.innerHTML += blockimages(false);
+    droite.innerHTML += blockimages(true);
+}
